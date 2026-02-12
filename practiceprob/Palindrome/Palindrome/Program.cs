d7924ef458af236219ff3adf9f967196da1c9e98
Aryan4646 <aryansha2004@gmail.com>
2026-02-12 11:25:32 +0530

    Practice problems

---
+++ practiceprob/Palindrome/Palindrome/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Welcome to liril what is the case madam";
+            string[] each_word = str.Split(' ');
+            string new_str = "";
+            int count = 0;
+            foreach (string i in each_word)
+            {
+                string new_ch = "";
+                for (int j = (i.Length) - 1; j >= 0; j--)
+                {
+                    new_ch = new_ch + i[j];
+                }
+                if (new_ch == i)
+                {
+                    count += 1;
+                }
+            }
+            Console.WriteLine("String is: " + str);
+            Console.WriteLine("Number of palindrome: " + count);
+        }
+    }
+}
---
+++ practiceprob/RevWord/RevWord/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevWord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                string str = "Welcome to C# programming from wipro training";
+                string[] each_word = str.Split(' ');
+                string new_str = "";
+
+                for (int i = 0; i < each_word.Length; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        new_str = new_str + each_word[i] + " ";
+                    }
+                    else
+                    {
+                        string new_ch = "";
+                        string word = each_word[i];
+                        for (int j = (word.Length) - 1; j >= 0; j--)
+                        {
+                            new_ch = new_ch + word[j];
+                        }
+                        new_str = new_str + new_ch + " ";
+                    }
+                }
+                Console.WriteLine(new_str);
+            }
+        }
+    }
+}
